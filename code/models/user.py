b16083155b36bcdb31b99c16b71633e7d0aa3cde
roy1210 <rswkure@gmail.com>
2021-10-31 02:01:59 +0800

    feat: add sql_alchemy

--- code/models/user.py
+++ code/models/user.py
@@ -1,46 +1,27 @@
 import sqlite3
+from db import db
 
 
-class UserModel:
-    def __init__(self, _id, username, password):
-        # _id -> python's variable
-        self.id = _id
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
     # not using 'self', make 'cls' as User class
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-
-        # query parameter has to tuple. Do (var,) -> add ',' in the end to tell python this is tuple. (if only 1 paramator)
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            # same as 'cls(row[0], row[1], row[2])'
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
--- code/resources/item.py
+++ code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource,  reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -11,6 +10,12 @@ class Item(Resource):
                         type=float,
                         required=True,
                         help='This field cannot be left blank'
+                        )
+
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help='Every item needs a store id.'
                         )
 
     @jwt_required()
@@ -26,56 +31,45 @@ class Item(Resource):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        # item = ItemModel(name, data['price'], data['store_id'])
+        print(data)
+        # print(**data)
+        item = ItemModel(name, **data)
 
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {"message": "An error occurred inserting the item."}, 500
 
         return item.json(), 201
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
 
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
         return {'message': 'Item deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {'message': 'An error occurred inserting the item'}, 500
+            item = ItemModel(name, data["price"], data['store_id'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {'message': 'An error occurred updating the item'}, 500
+            item.price = data['price']
 
-        return updated_item.json()
+        item.save_to_db()
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        # return {'items': item.json() for item in ItemModel.query.all()}
 
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
+        # map returns object in python (?)
+        #   return list(map(lambda x: x.json(), ItemModel.query.all()))
 
-        items = []
-        for row in result:
-            items.append({"name": row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        # returns by Array:
+        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
+        return {'items': [x.json() for x in ItemModel.query.all()]}
